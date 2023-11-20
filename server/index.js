5356ca63a3f024c5abb9ad60010879e2f8fd60ca
piyush <piyushchandra9458@gmail.com>
2023-11-20 06:01:52 -1200

    update3

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
-// import User from '/server/users'
 const app=express();
 app.use(cors());
 
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/heliverse')}   
    main()
-   // it is like .then .catch for catch the error
    const  db=mongoose.connection;
    db.on("error",console.error.bind(console, "connection error"));
    db.once("open",()=>{
@@ -27,9 +25,9 @@ async function main() {
     available:"String"
 })
 const User = mongoose.model('User', userSchema);
-// const card = JSON.stringify(Users);
+
    const users = await User.find({}).exec();
-    // console.log(users);
+    
  
   app.get('/',(req,res)=>{
     res.send(users)
